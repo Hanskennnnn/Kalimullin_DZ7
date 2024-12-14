fba47b1686d73301a95e8ce6ff0cbaf91826b3d7
Hanskennnnn <almazcheat@mail.ru>
2024-12-15 02:07:19 +0300

    final (Tumakov)

--- Tumakov_DZ/Program.cs
+++ Tumakov_DZ/Program.cs
@@ -12,12 +12,12 @@ namespace Tumakov_DZ
     {
         static void Main(string[] args)
         {
-            //Task1();
-            //Task2();
-            //Task3();
-            //Task4();
-            //Task5();
-            //Task6();
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+            Task6();
             Task7();
         }
         static  void Task1()
